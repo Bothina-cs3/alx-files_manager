5b33cf783d98d762d333bd2ca4c81a344a68d8d8
Bothina-cs3 <bothinamohammed944@gmail.com>
2024-12-11 11:39:25 -0800

    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-import express from 'express';
-import AppController from '../controllers/AppController.js';
+// routes/index.js
+const express = require('express');
+const AppController = require('../controllers/AppController');
 
 const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-export default router;
+module.exports = router;
